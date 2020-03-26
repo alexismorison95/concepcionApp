f103e44d18eb6ebb837ee17003c87fb558fa245b
alexismorison95 <alexismorison83@gmail.com>
2020-03-26 15:59:19 -0300

    NegocioFragment terminado

--- app/src/main/java/com/morris/concepcionapp/ui/main/NoticiasFragment.kt
+++ app/src/main/java/com/morris/concepcionapp/ui/main/NoticiasFragment.kt
@@ -1,17 +1,29 @@
 package com.morris.concepcionapp.ui.main
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.*
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.view.get
 
 import com.morris.concepcionapp.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.logging.SimpleFormatter
 
 /**
  * A simple [Fragment] subclass.
@@ -19,43 +31,105 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class NoticiasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var btnAtras: Button
+    private lateinit var btnAdelante: Button
+    private lateinit var btnReload: Button
+    private var urlMatutino: String? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_noticias, container, false)
+
+        loadViews(view)
+
+        loadPDF()
+
+        return view
+    }
+
+    private fun loadViews(view: View) {
+        webView = view.findViewById(R.id.noticias_webView)
+        progressBar = view.findViewById(R.id.progressBar)
+        btnAtras = view.findViewById(R.id.btnAtras)
+        btnAdelante = view.findViewById(R.id.btnAdelante)
+        btnReload = view.findViewById(R.id.btnReload)
+    }
+
+    private fun setListneresBtns(wv: WebView) {
+        btnAtras.setOnClickListener {
+            if (wv.canGoBack()) {
+                wv.goBack()
+            }
+        }
+
+        btnAdelante.setOnClickListener {
+            if (wv.canGoForward()) {
+                wv.goForward()
+            }
+        }
+
+        btnReload.setOnClickListener {
+            wv.reload()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_noticias, container, false)
+    private fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+        val formatter = SimpleDateFormat(format, locale)
+        return formatter.format(this)
+    }
+
+    private fun getCurrentDateTime(): Date {
+        return Calendar.getInstance().time
+    }
+
+    private fun loadPDF() {
+        webView.webViewClient = MyWebClient()
+        webView.settings.setSupportZoom(true)
+        webView.settings.javaScriptEnabled = true
+
+        // Debido a error al entrar en la pagina del gobierno
+        //webView.webChromeClient = MyWebChromeClient()
+
+        // Seteo la navegacion de los botones
+        setListneresBtns(webView)
+
+        // Fecha Actual
+        val date = getCurrentDateTime()
+        val matutinoFecha = date.toString("dd-MM")
+        //val viserinoFecha = date.toString("dd-MM-yy")
+
+        urlMatutino = "https://www.argentina.gob.ar/sites/default/files/covid19_informe-diario-matutino-$matutinoFecha.pdf"
+        //val urlVisperino = "https://www.argentina.gob.ar/sites/default/files/$viserinoFecha-reporte-diario-vespertino-covid-19.pdf"
+
+        webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=$urlMatutino")
+
+        // webView.loadUrl(urlVisperino)
+    }
+
+    // Nuevo WebClient para poner un progressBar
+    inner class MyWebClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            progressBar.visibility = View.VISIBLE
+            view.loadUrl(url)
+            return true
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+            progressBar.visibility = View.GONE
+        }
+
+        override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+            super.onReceivedError(view, request, error)
+            webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=$urlMatutino")
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoticiasFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoticiasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = NoticiasFragment()
     }
 }
